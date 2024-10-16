54a6ab7a57c6b633bc254015078c5cd0dde4e9ee
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2024-10-16 11:44:50 +0200

    Add mongoose and set up configs for mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { getMongoConfig } from './configs/mongo.config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		ConfigModule.forRoot(),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: getMongoConfig,
+			inject: [ConfigService],
+		}),
+	],
+	controllers: [AppController],
+	providers: [AppService],
+	exports: [ConfigModule],
 })
 export class AppModule {}
