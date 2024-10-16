631553b4d7b968df2f2498fb6a63d4b749659abe
Mikhail Kultyiasau <mikhail.kultiyasov@tispr.com>
2024-10-16 11:51:00 +0200

    Add basic swagger configs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	// Swagger configuration
+	const config = new DocumentBuilder()
+		.setTitle('Steps Leaderboard API')
+		.setDescription('API for managing teams and step counters')
+		.setVersion('1.0')
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('explore', app, document);
+
+	await app.listen(3000);
 }
 bootstrap();
